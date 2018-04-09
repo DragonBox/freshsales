49e0d884a95471c34e575e4df28aa2456bde3873
Jerome Lacoste <jerome.lacoste@gmail.com>
2018-04-09 08:23:27 +0200

    Handle Freshsales errors (fixes #3)

--- lib/freshsales/client.rb
+++ lib/freshsales/client.rb
@@ -27,11 +27,12 @@ module Freshsales
       parsed_response
     end
 
-    def jsonify_body(body)
+    def jsonify_body(body, ignore_parsing_errors: false)
       MultiJson.load(body, symbolize_keys: @config.symbolize_keys)
-    rescue MultiJson::ParseError
-      error_params = { title: "UNPARSEABLE_RESPONSE", status_code: 500 }
-      error = FreshsalesError.new("Unparseable response: '#{body}'", error_params)
+    rescue MultiJson::ParseError => e
+      return if ignore_parsing_errors
+      error_params = { detail: e.message, status_code: 500, raw_body: body }
+      error = FreshsalesError.new("Unparseable response", error_params)
       raise error
     end
 
@@ -50,6 +51,34 @@ module Freshsales
         end
         request.url path
       end
+    rescue => e
+      handle_request_error(e)
+    end
+
+    def handle_request_error(error)
+      error_params = {}
+      if error.is_a?(Faraday::Error::ClientError) && error.response
+        error_params[:status_code] = error.response[:status]
+        error_params[:raw_body] = error.response[:body]
+
+        parsed_response = jsonify_body(error.response[:body], ignore_parsing_errors: true)
+
+        binding.pry
+
+        if parsed_response
+          error_params[:body] = parsed_response
+
+          code_key = @config.symbolize_keys ? :code : "code"
+          message_key = @config.symbolize_keys ? :message : "message"
+
+          error_params[:code] = parsed_response[code_key] if parsed_response[code_key]
+          error_params[:detail] = parsed_response[message_key] if parsed_response[message_key]
+        end
+      end
+      message = error_params[:detail] || error.message
+      error_to_raise = FreshsalesError.new(message, error_params)
+
+      raise error_to_raise
     end
 
     def connection
